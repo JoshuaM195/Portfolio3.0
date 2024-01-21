0364dedb92948b997b740ac0936960df435b94c0
JoshuaM195 <joshuamathew195@gmail.com>
2024-01-21 15:39:17 -0600

    map project details onto cards

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
+import Image from "next/image";
 
 export default function Projects() {
   return (
@@ -20,5 +21,16 @@ export default function Projects() {
 type ProjectProps = (typeof projectsData)[number];
 
 function Project({ title, description, tags, imageUrl }: ProjectProps) {
-  return <div>{title}</div>;
+  return (
+    <section className="bg-gray-100 max-w-[42rem] border border-black/5">
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <ul>
+        {tags.map((tag, index) => (
+          <li key={index}>{tag}</li>
+        ))}
+      </ul>
+      <Image src={imageUrl} alt="Project I worked on" quality={95} />
+    </section>
+  );
 }
