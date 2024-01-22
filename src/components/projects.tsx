76a78b47597237ba66f5c5fa4da10cd67a2fa473
JoshuaM195 <joshuamathew195@gmail.com>
2024-01-21 20:36:28 -0600

    image input

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -22,15 +22,27 @@ type ProjectProps = (typeof projectsData)[number];
 
 function Project({ title, description, tags, imageUrl }: ProjectProps) {
   return (
-    <section className="bg-gray-100 max-w-[42rem] border border-black/5">
-      <h3>{title}</h3>
-      <p>{description}</p>
-      <ul>
-        {tags.map((tag, index) => (
-          <li key={index}>{tag}</li>
-        ))}
-      </ul>
-      <Image src={imageUrl} alt="Project I worked on" quality={95} />
+    <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative">
+      <div className="py-4 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
+        <h3 className="text-2xl font-semibold">{title}</h3>
+        <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
+        <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
+          {tags.map((tag, index) => (
+            <li
+              className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
+              key={index}
+            >
+              {tag}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <Image
+        src={imageUrl}
+        alt="Project I worked on"
+        quality={95}
+        className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl"
+      />
     </section>
   );
 }
