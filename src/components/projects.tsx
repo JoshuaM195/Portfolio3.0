922e97d02f5b7381b61b0a77753305d538892dc7
JoshuaM195 <joshuamathew195@gmail.com>
2024-01-30 10:58:47 -0600

    change

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -63,7 +63,7 @@ function PhoneProject({ title, description, tags, imageUrl }: ProjectProps) {
 
 function ComputerProject({ title, description, tags, imageUrl }: ProjectProps) {
   return (
-    <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative">
+    <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem]">
       <div className="py-4 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
         <h3 className="text-2xl font-semibold">{title}</h3>
         <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
@@ -78,22 +78,12 @@ function ComputerProject({ title, description, tags, imageUrl }: ProjectProps) {
           ))}
         </ul>
       </div>
+
       <Image
         src={imageUrl}
         alt="Project I worked on"
         quality={95}
-        className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
-          transition 
-          group-hover:scale-[1.04]
-          group-hover:-translate-x-3
-          group-hover:translate-y-3
-          group-hover:-rotate-2
-  
-          group-even:group-hover:translate-x-3
-          group-even:group-hover:translate-y-3
-          group-even:group-hover:rotate-2
-  
-          group-even:right-[initial] group-even:-left-40"
+        className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl"
       />
     </section>
   );
