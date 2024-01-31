07308637ceb68b55ebc8c640b12d9c47adf16bc9
JoshuaM195 <joshuamathew195@gmail.com>
2024-01-30 22:23:34 -0600

    fixed framing and div

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -24,7 +24,7 @@ export default function Projects() {
   );
 }
 
-type ProjectProps = typeof projectsData[number];
+type ProjectProps = (typeof projectsData)[number];
 
 function PhoneProject({ title, description, tags, imageUrl }: ProjectProps) {
   return (
@@ -63,7 +63,7 @@ function PhoneProject({ title, description, tags, imageUrl }: ProjectProps) {
 
 function ComputerProject({ title, description, tags, imageUrl }: ProjectProps) {
   return (
-    <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem]">
+    <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative">
       <div className="py-4 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
         <h3 className="text-2xl font-semibold">{title}</h3>
         <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
