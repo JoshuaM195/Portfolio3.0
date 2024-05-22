347680b72418b4e0853978f1fc0eb4ef887b3be7
JoshuaM195 <joshuamathew195@gmail.com>
2024-05-22 00:57:06 -0500

    Finished Header

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
 import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
 import Project from "./project";
+import { useActiveSectionContext } from "../../context/active-section-context";
+import { useInView } from "react-intersection-observer";
 
 export default function Projects() {
+  const { ref, inView } = useInView({
+    threshold: 0.5,
+  });
+  const { setActiveSection } = useActiveSectionContext();
+  useEffect(() => {
+    if (inView) {
+      setActiveSection("Projects");
+    }
+  }, [inView, setActiveSection]);
+
   return (
-    <section id="projects" className="scroll-mt-28">
+    <section ref={ref} id="projects" className="scroll-mt-28">
       <SectionHeading>My Projects</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
