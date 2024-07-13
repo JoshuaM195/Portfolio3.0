e06552abe13716996a619e00a56142aa6ea8e0e0
JoshuaM195 <joshuamathew195@gmail.com>
2024-07-13 01:59:08 -0500

    Implement contact form and button with style

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,17 +1,33 @@
 import React from "react";
 import SectionHeading from "./section-heading";
+import { FaPaperPlane } from "react-icons/fa";
 
 export default function Contact() {
   return (
-    <section id="contact">
+    <section id="contact" className="mb-20 sm:mb-28 w-[min(100%,38rem)]">
       <SectionHeading>Contact Me</SectionHeading>
-      <p>
+      <p className="text-gray-700">
         Please contact me directly at{" "}
         <a className="underline" href="mailto:joshuamathew195@gmail.com">
           joshuamathew195@gmail.com
         </a>{" "}
         or through this form.
       </p>
+
+      <form className="mt-10 flex flex-col">
+        <input className="h-14 rounded-lg borderBlack" type="email" />
+        <textarea className="h-52 my-3 rounded-lg borderBlack p-4" />
+        <button
+          className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105"
+          type="submit"
+        >
+          Submit{" "}
+          <FaPaperPlane
+            className="text-xs opacity-70 transition-all group-hover:translate-x-1 
+          group-hover:-translate-y-1"
+          />{" "}
+        </button>
+      </form>
     </section>
   );
 }
