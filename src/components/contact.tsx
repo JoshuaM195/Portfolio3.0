03f582695d56d33139b50a9ea72563b42e568f40
JoshuaM195 <joshuamathew195@gmail.com>
2024-07-13 17:03:30 -0500

    design contact section

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,22 +1,43 @@
+"use client";
+
 import React from "react";
 import SectionHeading from "./section-heading";
 import { FaPaperPlane } from "react-icons/fa";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
 
 export default function Contact() {
+  const { ref } = useSectionInView("Contact");
+
   return (
-    <section id="contact" className="mb-20 sm:mb-28 w-[min(100%,38rem)]">
+    <motion.section
+      id="contact"
+      ref={ref}
+      className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      viewport={{ once: true }}
+    >
       <SectionHeading>Contact Me</SectionHeading>
-      <p className="text-gray-700">
+      <p className="text-gray-700 -mt-6">
         Please contact me directly at{" "}
         <a className="underline" href="mailto:joshuamathew195@gmail.com">
           joshuamathew195@gmail.com
         </a>{" "}
-        or through this form.
+        or here.
       </p>
 
       <form className="mt-10 flex flex-col">
-        <input className="h-14 rounded-lg borderBlack" type="email" />
-        <textarea className="h-52 my-3 rounded-lg borderBlack p-4" />
+        <input
+          className="h-14 px-4 rounded-lg borderBlack"
+          type="email"
+          placeholder="Your email"
+        />
+        <textarea
+          className="h-52 my-3 rounded-lg borderBlack p-4"
+          placeholder="Your message"
+        />
         <button
           className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105"
           type="submit"
@@ -28,6 +49,6 @@ export default function Contact() {
           />{" "}
         </button>
       </form>
-    </section>
+    </motion.section>
   );
 }
